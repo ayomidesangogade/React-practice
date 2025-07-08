0202f934921b3af55479e073da6173ad43e44f9a
Sangogade Ayomide <ayomideoluwagade@gmail.com>
2025-07-08 23:01:49 +0100

    Refactor Pad component to use props for 'on' state; update Pads component to correctly toggle pad states

--- react-practice/src/Pad.jsx
+++ react-practice/src/Pad.jsx
@@ -1,15 +1,14 @@
 import React from "react"
 
 function Pad(props) {
-    const [on, setOn] = React.useState(props.on)
 
-    function ChangeColour() {
-        setOn(prev => !prev)
-    }
+    // function ChangeColour() {
+    //     setOn(prev => !prev)
+    // }
     return (
         <button 
             style={{backgroundColor: `${props.color}`}} 
-            className={on ? "on" : undefined} 
+            className={props.on ? "on" : undefined} 
             onClick={() => props.toggle(props.id)}
         >
         </button>
--- react-practice/src/Pads.jsx
+++ react-practice/src/Pads.jsx
@@ -6,8 +6,12 @@ export default function Pads() {
     const [statePads, setStatePads] = React.useState(pads)
 
     function ToggleFunction(id) {
-        // map over the pads array, and if the current item has the same id as the one passed to this function, then flip its "on" value
-        setStatePads(prev => [...prev, {...prev[id], on: !prev[id].on}])
+        
+        setStatePads(prevPads =>
+            prevPads.map(pad =>
+            pad.id === id ? { ...pad, on: !pad.on } : pad
+            )
+        )
     }
 
     const buttonElements = statePads.map(element => <Pad key={element.id} id={element.id} color={element.color} on={element.on} toggle={ToggleFunction} />)
